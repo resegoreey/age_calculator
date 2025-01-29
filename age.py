a5981cda14acc82343838cd52fade7c1b0fd5439
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-01-29 21:59:11 +0200

    Wrote basic code for age calculation

---
+++ age.py
@@ -0,0 +1,5 @@
+birth_year = int(input("Enter your birth year: "))
+current_year = int(input("Enter the current year: "))
+age = current_year - birth_year
+
+print(f"You are {age} years old on in {current_year}")
