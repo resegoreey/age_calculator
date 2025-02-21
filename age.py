a7f91e2dce871442d50657fe9312703201969588
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-02-21 20:49:39 +0200

    Placed the code in a function

--- age.py
+++ age.py
@@ -1,5 +1,7 @@
-birth_year = int(input("Enter your birth year: "))
-current_year = int(input("Enter the current year: "))
-age = current_year - birth_year
+def calculate_age():
+    birth_year = int(input("Enter your birth year: "))
+    current_year = int(input("Enter the current year: "))
+    age = current_year - birth_year
 
-print(f"You are {age} years old on in {current_year}")
+    print(f"You are {age} years old on in {current_year}")
+calculate_age()
